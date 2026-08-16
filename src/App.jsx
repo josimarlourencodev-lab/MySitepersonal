67a0dfc80a12689732d20a1171fbe28aa4b6c49d
josimarlourencodev-lab <josimarlourencodev@gmail.com>
2026-08-16 09:02:39 -0300

    feat: init repo
    esse commit foi feito na inteção de iniciar repo

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+         <h1>Josimar Chagas</h1>
+         <h3>Sou estudante de desenvolvimento de software</h3>
+      </div>
+      <div>
+       <p>
+          Sou Josimar Chagas Lourenço, estudante técnico de Informática no Brasil. <br />
+
+          Apaixonado por tecnologia e por criar soluções inteligentes e criativas. <br />
+
+          Certificado em Análise de Dados pelo Google, também atuo com computação em nuvem (AWS). <br />
+
+          Gosto de desenvolver soluções com IA, e estou me aprofundando em Machine Learning. <br />
+
+          Lidero a criação do TRUD, um ERP open source para diferentes negócios. 
+      
+        </p>
+      </div>
+    
+
+    </>
+  )
+}
+
+export default App
